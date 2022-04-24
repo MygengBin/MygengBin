b4ed04471ecaf889fed2633775bf4d524db72ab1
gengbin <1371103410@qq.com>
2022-04-24 21:49:48 +0800

    delete  dist

--- dist/js/about.34230746.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkmygengbin"]=self["webpackChunkmygengbin"]||[]).push([[443],{4335:function(n,e,u){u.r(e),u.d(e,{default:function(){return f}});var t=u(3396);const a={class:"about"},r=(0,t._)("h1",null,"This is an about page",-1),s=[r];function i(n,e){return(0,t.wg)(),(0,t.iD)("div",a,s)}var c=u(89);const o={},b=(0,c.Z)(o,[["render",i]]);var f=b}}]);
-//# sourceMappingURL=about.34230746.js.map
