39b039376e555a24ffb04f104de60bf6c66f66c9
gengbin <1371103410@qq.com>
2022-04-25 21:44:04 +0800

    web front check system theme

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 - 💞️ I’m looking to collaborate on None.
 - 📫 How to reach me Java script. learing...
 - [Nginx CORS](./nginx跨域)
+- [Front end Check System Theme](./markdown/%E5%89%8D%E7%AB%AF%E5%88%A4%E6%96%AD%E7%B3%BB%E7%BB%9F%E4%B8%BB%E9%A2%98.md)
 <!---
 MygengBin/MygengBin is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
 You can click the Preview link to take a look at your changes.
--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,66 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
+  <div>
+    <button @click="black">黑色</button>
+    <button @click="white">白色</button>
+    <button @click="auto">跟随系统</button>
+  </div>
   <router-view/>
 </template>
+<script lang="ts">
+import {defineComponent} from 'vue';
+const {body} = document;
+export default defineComponent({
+  methods:{
+    black(){
+      !body.classList.contains('dark') && body.classList.add('dark');
+      body.classList.contains('light') && body.classList.remove('light');
+    },
+    white(){
+      !body.classList.contains('light') && body.classList.add('light');
+      body.classList.contains('dark') && body.classList.remove('dark');
+    },
+    auto(){
+      body.classList.contains('light') && body.classList.remove('light');
+      body.classList.contains('dark') && body.classList.remove('dark');
+    },
+  }
+});
+</script>
 
 <style lang="scss">
+@media (prefers-color-scheme: dark) {
+    body {
+      background: rgb(53, 54, 58);
+      color: rgba(238,238,238,1);
+    }
+    nav{
+    a{
+        color:white;
+        &.router-link-exact-active {
+          color: gray;
+        }
+      }
+  }
+}
+
+@media (prefers-color-scheme: light) {
+    body {
+      background: rgb(255,255,255);
+      color: rgba(51,51,51,1);
+    }
+    nav {
+        a {
+          
+          color: #2c3e50;
+
+          &.router-link-exact-active {
+            color: #42b983;
+          }    
+        }
+      }
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -14,16 +70,39 @@
   text-align: center;
   color: #2c3e50;
 }
+body{
+  &.dark{
+    background: rgb(53, 54, 58);
+    color: rgba(238,238,238,1);
+    h1,h2,h3,h4,h5,h6,p{
+      color:white;
+    }
+    nav{
+      a{
+        color:white;
+        &.router-link-exact-active {
+          color: gray;
+        }
+      }
+    }
+  }      
+  &.light{
+    background: rgb(255,255,255);
+    color: rgba(51,51,51,1);
+    nav {
+      a {
+        color: #2c3e50;
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+        &.router-link-exact-active {
+          color: #42b983;
+        }    
+      }
+    }
+  }
+  nav{
+    padding: 30px;
+    a{
+      font-weight: bold;
     }
   }
 }
